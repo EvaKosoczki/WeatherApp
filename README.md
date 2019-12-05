7d896cc19e7ba36f636dd9f6f83b58b6dd2590ad
evakosoczki <eva.kosoczki@gmail.com>
2019-12-05 16:31:49 +0100

    statistical functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WeatherApp
+# https://www.iconfinder.com/iconsets/sketchy-weather-icons-by-azuresol
--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ function onClick() {
             return response.json();
         })
         .then(function (data) {
+            forecastDays(data);
             maxTempCalculator(data);
             minTempCalculator(data);
             weatherDescription(data);
+
             console.log(data);
         });
     document.querySelector("#cityName").innerHTML = searchText;
@@ -26,10 +28,11 @@ function maxTempCalculator(data) {
 
         }
         else {
-            maxTemperatures.push(maxTemp);
+            maxTemperatures.push("Maximum: " + maxTemp + " °C");
         }
     }
     console.log(maxTemperatures);
+    tempWriter(maxTemperatures)
 }
 
 function minTempCalculator(data) {
@@ -44,30 +47,51 @@ function minTempCalculator(data) {
 
         }
         else {
-            minTemperatures.push(minTemp);
+            minTemperatures.push("Minimum: " + minTemp + " °C");
         }
     }
     console.log(minTemperatures);
+    tempWriter(minTemperatures);
 }
 
 function weatherDescription(data) {
-    //descriptions.push(data.list[i].weather[0].description)
     let descriptions = [];
-
+    let cardLeftCols = document.querySelectorAll(".leftcol");
     for (let i = 0, j = 1; i, j < data.list.length; i += 1, j += 1) {
 
         if (new Date(data.list[i].dt_txt).getUTCDate() == (new Date(data.list[j].dt_txt)).getUTCDate()) {
             if (new Date(data.list[i].dt_txt).getHours() == 12) {
                 descriptions.push(data.list[i].weather[0].description);
-
             }
-
         }
-
+    }
+    for (let i = 1, j = 0; i < descriptions.length, j < cardLeftCols.length; i += 1, j += 1) {
+        let image = document.createElement('img');
+        image.src = "./img/clear.png";
+        cardLeftCols[j].appendChild(image);
+        let weatherDesc = document.createElement('p');
+        cardLeftCols[j].appendChild(weatherDesc);
+        weatherDesc.innerHTML = descriptions[i];
     }
 
     console.log(descriptions);
 };
+
+function forecastDays(data) {
+    let dates = Array.from(new Set(data.list.map(item => item.dt_txt.slice(5, 10))));
+    let cardHeaders = document.querySelectorAll(".card-header");
+    for (let i = 1, j = 0; i < dates.length, j < cardHeaders.length; i += 1, j += 1) {
+        cardHeaders[j].innerHTML = dates[i];
+    }
+}
+
+function tempWriter(dataset) {
+    let cardRightCol = document.querySelectorAll(".rightcol");
+    for (let i = 1, j = 0; i < dataset.length, j < cardRightCol.length; i += 1, j += 1) {
+        cardRightCol[j].innerHTML += dataset[i];
+        cardRightCol[j].innerHTML += "<br>";
+    }
+}
 
 $(function () {
     $('.toggle').on('click', function () {
