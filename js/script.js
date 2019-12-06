28590979a371142267aa2c2a37ddfb6fcd32d1ff
evakosoczki <eva.kosoczki@gmail.com>
2019-12-06 12:28:12 +0100

    sth

--- js/script.js
+++ js/script.js
@@ -93,36 +93,11 @@ function tempWriter(dataset) {
     }
 }
 
-$(function () {
-    $('.toggle').on('click', function () {
-
-        var isMaximizing = $(this).hasClass('fa-expand') === true;
-        var card = $(this).parent('.card');
-        var animationDelay = 500; // in ms
-
-        /**
-          Ordering of animations is different depending on if we maximize or minimize a card
-        **/
-        if (isMaximizing) {
-            card.toggleClass('top');
-            $('.card').toggleClass('center');
-            // Wait for cards to center, then maximize
-            setTimeout(function () {
-                card.toggleClass('card-small card-large');
-            }, animationDelay);
-        } else {
-            card.toggleClass('card-small card-large');
-            // Wait until card has minized, then re-position cards
-            setTimeout(function () {
-                $('.card').toggleClass('center');
-
-                // Reset z-index after cards are in their rightful place
-                setTimeout(function () {
-                    card.toggleClass('top');
-                }, animationDelay);
-            }, animationDelay);
-        }
-
-        $(this).toggleClass('fa-expand fa-compress');
-    });
-});
+function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
