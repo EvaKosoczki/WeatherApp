dbc05325753788a5731ae51a50be0156113be03c
evakosoczki <eva.kosoczki@gmail.com>
2019-12-06 12:52:10 +0100

    new stuff

--- js/script.js
+++ js/script.js
@@ -92,12 +92,17 @@ function tempWriter(dataset) {
         cardRightCol[j].innerHTML += "<br>";
     }
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-function openTab(tabName) {
-    var i, x;
-    x = document.getElementsByClassName("containerTab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
     }
-    document.getElementById(tabName).style.display = "block";
+  });
 }
