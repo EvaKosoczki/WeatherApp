d9b9dff8770e52e6733294dfa1937ed183fc9edf
evakosoczki <eva.kosoczki@gmail.com>
2019-12-06 13:37:21 +0100

    working

--- js/script.js
+++ js/script.js
@@ -95,14 +95,25 @@ function tempWriter(dataset) {
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+function openTab(tabName) {
+    let content = document.getElementById(tabName)
+    content.classList.toggle("active");
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+        content.style.maxHeight = content.scrollHeight + "px";
     }
-  });
 }
+
+/*
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}*/
